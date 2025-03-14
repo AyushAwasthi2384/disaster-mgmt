5c8253eb4294e1b7341299aca9207b122a99326c
Hemang2208 <workwithhemang@gmail.com>
2025-03-14 15:19:00 +0530

    Updated UI

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,15 +3,15 @@ import User from "../models/user.model.js";
 const registerUser = async (req, res) => {
   try {
     const { name, email, password, userRole } = req.body;
-        const hashPassword = await User.hashPassword(password);
+    const hashPassword = await User.hashPassword(password);
 
     const user = await User.create({ name, email, hashPassword, userRole });
     const token = await User.generateAuthToken();
 
-        res.status(201).json({ message: "User create hogyaa!!", token, user });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+    res.status(201).json({ message: "User create hogyaa!!", token, user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 const getUsers = async (req, res) => {
