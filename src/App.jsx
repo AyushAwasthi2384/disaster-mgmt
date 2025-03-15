e02cd3fc79077182edcffa7d702e5303702adb35
Shivangi-128-bit <mishrashivangi3232@gmail.com>
2025-03-16 00:59:39 +0530

    Added Community component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SignupPage from "./pages/Signup";
 import LandingPage from "./pages/LandingPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import DisasterDashboard from "./pages/Dashboard";
+import Community from "./mobileApp/Community";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<NotFoundPage />} />
+        <Route path="/Community" element={<Community />} /> 
       </Routes>
     </Router>
   );
